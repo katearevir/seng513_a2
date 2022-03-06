e030eff7594d2d85ece1e666742aaf15a6cc586d
Kate Rivera <kateashley.rivera@ucalgary.ca>
2022-03-06 15:52:35 -0700

    nickname has connected

---
+++ client.js
@@ -0,0 +1,34 @@
+
+var socket = io();
+
+//nickname
+var register = document.getElementById('register');
+var nickname = document.getElementById('nickname');
+
+register.addEventListener('submit', function(e) {
+    e.preventDefault();
+    socket.emit('nickname', input.value);
+});
+
+socket.on('nickname', function(name) {
+    console.log("nickname: " + name);
+});
+
+//index
+var form = document.getElementById('form');
+var input = document.getElementById('input');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+            if (input.value) {
+                socket.emit('chat message', input.value);
+                input.value = '';
+            }
+        });
+
+        socket.on('chat message', function(msg) {
+            var item = document.createElement('li');
+            item.textContent = msg;
+            messages.appendChild(item);
+            window.scrollTo(0, document.body.scrollHeight);
+        });
--- server.js
+++ server.js
@@ -4,15 +4,35 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const bodyParser = require('body-parser');
 
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var jsonParser = bodyParser.json();
+
+var defaultNames = ["Bob", "Alice", "John", "Jane"];
+var takenNames = [];
+
+var tmp;
 //send html doc to server
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/nickname.html');
+});
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/register', urlencodedParser, (req, res) => {
+  tmp = req.body.nickname;
+  takenNames.push(tmp);
+  console.log(takenNames);
+  res.redirect('/chat');
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-    io.emit('chat message', socket.id + " has connected.");
+    io.emit('chat message', tmp + " has connected.");
+    //io.emit('chat message', socket.id + " has connected.");
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
