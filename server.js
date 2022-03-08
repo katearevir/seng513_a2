4011e626c1a0004f5c3f3e3215d513504ba8999c
Kate Rivera <kateashley.rivera@ucalgary.ca>
2022-03-07 23:41:34 -0700

    connect user name colour

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.use(express.static(__dirname + '/public'));
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 var jsonParser = bodyParser.json();
 
-var defaultNames = ["Bob", "Alice", "John", "Jane"];
+var defaultNames = ["Bob", "Alice", "John", "Jane", "Charlie", "Dakota", "River"];
 var takenNames = [];
+var colours = [];
 
-var tmp;
+var tmpName;
+var tmpColour;
 //send html doc to server
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -25,28 +27,49 @@ app.get('/chat', (req, res) => {
 });
 
 app.post('/register', urlencodedParser, (req, res) => {
-  tmp = req.body.nickname;
-  takenNames.push(tmp);
-  console.log(takenNames);
+  tmpName = req.body.nickname;
+  tmpColour = req.body.colour;
 
+  if (tmpName == ""){
+    tmpName = defaultNames[0];
+    defaultNames.shift();
+  }
   res.redirect('/chat');
+  
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    io.emit('chat message', tmp + " has connected.");
-    //io.emit('chat message', socket.id + " has connected.");
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
+  console.log('a user connected' + socket.id);
+
+  if (tmpName !== "" && typeof tmpName !== "undefined")
+  {
+    takenNames[socket.id] = tmpName;
+    colours[socket.id] = tmpColour;
+    tmpName = "";
+    io.emit('user connect', takenNames[socket.id] + " has connected.");
+    io.emit('user list', takenNames[socket.id]);
+    console.log(takenNames);
+    console.log(colours);
+  }
   
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      console.log('message: ' + msg);
-      io.emit('chat message', tmp + " " + msg);
-    });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (message) => {
+    console.log('message: ' + message);
+    var details={
+      name: takenNames[socket.id],
+      colour: colours[socket.id],
+      msg: message
+    };
+    io.emit('chat message', details);
+  });
+});
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
